4f7e18095ede58a58863d1fca50443cfde01e465
Connor Segneri <connorsegneri@gmail.com>
2019-08-27 15:58:26 -0700

    create a stop watch class exercise

---
+++ C#Intermediate/DesignAStopwatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DesignAStopwatch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch sp = new StopWatch();
+
+            //sp.Stop();
+
+            //Console.WriteLine(sp.GetDuration().ToString("c"));
+
+            //sp.Start();
+            //Console.WriteLine(sp.GetDuration().ToString("c"));
+
+            sp.Start();
+            sp.Stop();
+            Console.WriteLine(sp.GetDuration().ToString("c"));
+        }
+    }
+}
---
+++ C#Intermediate/DesignAStopwatch/StopWatch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DesignAStopwatch
+{
+    class StopWatch
+    {
+        private DateTime StartTime;
+        private DateTime EndTime;
+        private bool IsStopWatchRunning;
+
+        public StopWatch()
+        {
+            this.IsStopWatchRunning = false;
+        }
+
+        public void Start()
+        {
+            if (!this.IsStopWatchRunning)
+            {
+                this.StartTime = DateTime.Now;
+                this.IsStopWatchRunning = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("StopWatch is already running");
+            }
+        }
+
+        public void Stop()
+        {
+            if (this.IsStopWatchRunning)
+            {
+                this.EndTime = DateTime.Now;
+                this.IsStopWatchRunning = false;
+            }
+            else
+            {
+                throw new InvalidOperationException("StopWatch is not running");
+            }
+        }
+
+        public TimeSpan GetDuration()
+        {
+            if (!this.IsStopWatchRunning)
+            {
+                return this.EndTime - this.StartTime;
+            }
+            else
+            {
+                throw new InvalidOperationException("StopWatch is running");
+            }
+        }
+    }
+}
